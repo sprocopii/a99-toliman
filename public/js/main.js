ca608bdb74080dcdf90c71618ba6720b05d10000
Stephan Procopii <stephanp@live.unc.edu>
2022-04-30 11:28:40 -0400

    fixed some stuff

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,17 @@
-// js file for html and css content
+/// js file for html and css content
 
 // PHYSICAL HEALTH SCRIPTS //
+// get physical task start //
 const physicaltask=document.getElementById("physicaltask")
+
 // event listener for click
 physicaltask.addEventListener("click", getPhysTasks)
 // function to wait for response
+
 async function getPhysTasks() {
     // endpoint
-    const endpoint="/app/physical"
-    const url=document.BASEURI+endpoint
+    const endpoint='/app/physical'
+    const url=document.baseURI+endpoint
     // waiting for response
     await fetch(url)
     // receiving response
@@ -16,31 +19,86 @@ async function getPhysTasks() {
                         return response.json();
                     })
                     
-                    .then(function(physicalres) {
-                        console.log(physicalres);
-                        document.getElementById("physicalres").innerHTML = physicalres.tasks;
+                    .then(function(result) {
+                        console.log(result);
+                        document.getElementById("physicalres").innerHTML = result;
             
                     });
-}
-// MENTAL HEALTH SCRIPTS
-const mentaltask = document.getElementById("mentaltask")
+} 
+// get phys task end
+
+// get description 1 //
+const ptask1=document.getElementById("ptask1")
+
 // event listener for click
-mentaltask.addEventListener("click", getMentalTasks)
+physicaltask.addEventListener("click", getTask1Des)
 // function to wait for response
-async function getPhysTasks() {
+
+async function getTask1Des() {
     // endpoint
-    const endpoint = "/app/mental"
-    const url = document.BASEURI+endpoint
+    const endpoint='/app/physical/task1'
+    const url=document.baseURI+endpoint
     // waiting for response
     await fetch(url)
     // receiving response
                     .then(function(response) {
                         return response.json();
                     })
-
-                    .then(function(mentalres) {
-                        console.log(mentalres);
-                        document.getElementById("mentalres").innerHTML = mentalres.tasks;
+                    
+                    .then(function(result) {
+                        console.log(result);
+                        document.getElementById("pdes1").innerHTML = result;
             
                     });
-}
+} 
+
+// get description 2 //
+const ptask2=document.getElementById("ptask2")
+
+// event listener for click
+physicaltask.addEventListener("click", getTask2Des)
+// function to wait for response
+
+async function getTask2Des() {
+    // endpoint
+    const endpoint='/app/physical/task2'
+    const url=document.baseURI+endpoint
+    // waiting for response
+    await fetch(url)
+    // receiving response
+                    .then(function(response) {
+                        return response.json();
+                    })
+                    
+                    .then(function(result) {
+                        console.log(result);
+                        document.getElementById("pdes2").innerHTML = result;
+            
+                    });
+} 
+
+
+// get description 3 //
+const ptask3=document.getElementById("ptask3")
+
+// event listener for click
+physicaltask.addEventListener("click", getTask3Des)
+// function to wait for response
+
+async function getTask3Des() {
+    // endpoint
+    const endpoint='/app/physical/task3'
+    const url=document.baseURI+endpoint
+    // waiting for response
+    await fetch(url)
+    // receiving response
+                    .then(function(response) {
+                        return response.json();
+                    })
+                    
+                    .then(function(result) {
+                        console.log(result);
+                        document.getElementById("pdes3").innerHTML = result;
+            
+                    });
+} 
