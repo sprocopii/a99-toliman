2089d91c0818ca1856e641138ca89dce62453e0d
Stephan Procopii <stephanp@live.unc.edu>
2022-05-01 18:52:49 -0400

    added some css for all pages, in process of adding more

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ const newuserdb = ((req, res, next) => {
 
 
 
-var physical_tasks = ["Walk 6k steps", "30 minutes of aerobics", "20 minutes of Yoga", "Go on a quick jog", "10 minutes of stretching", "15 minutes of biking"]
+var physical_tasks = ["Walk 6k steps", "\n30 minutes of aerobics", "\n20 minutes of Yoga", "\nGo on a quick jog", "\n10 minutes of stretching", "\n15 minutes of biking"]
 var physical_descriptions = ["Walking 6k steps a day is necessary to ensure the health of your joints and muscles!",
                             "30 minutes of aerobics is the heart's best treatment. It increases your stamina and ensures healthy arteries",
                             "20 minutes of yoga will improve your flexibility, reduce inflammation, and increase your strength",
@@ -56,12 +56,12 @@ var physical_descriptions = ["Walking 6k steps a day is necessary to ensure the 
                             "10 minutes of stretching is enough to keep muscles, joints, and bones in their proper alignments",
                             "15 minutes of biking is perfect for mobility, balance, flexibility, and strength"]
 
-var mental_tasks = ["Spend time outside", "30 minutes of reading", "Talk to a loved one on the phone", "10 minutes of journaling", "1 hour without your phone", "Watch a 20 minute show"]
+var mental_tasks = ["Spend time outside", "\n30 minutes of reading", "\nTalk to a loved one on the phone", "\n10 minutes of journaling", "\n1 hour without your phone", "\nWatch a 20 minute show"]
 var mental_descriptions = ["Spending time outside can help to reduce anxiety and depression. Take a break today and spend some time outside", 
                             "Reading can help improve your quality of sleep and sharpen you mind.", 
                             "Talking to loved ones daily can help increase your sense of belonging and boost your happiness.", 
-                            "Journaling helps you to express your thoughts and emotions while giving you a moment of relfection.", 
-                            "Give your brain a rest. Unplugging for an hour will help reduce stress and anxeity.", 
+                            "Journaling helps you to express your thoughts and emotions while giving you a moment of reflection.", 
+                            "Give your brain a rest. Unplugging for an hour will help reduce stress and anxiety.", 
                             "Take a moment to relax and watch an episode of your favorite TV show!"]
 
 
@@ -80,7 +80,7 @@ function date_Randomizer() {
     } else {
         var task1 = physical_tasks.at(1)
         var task2 = physical_tasks.at(3)
-        var task3 = physical_tasks.at(4)
+        var task3 = physical_tasks.at(5)
         return [task1, task2, task3]
     }
 }
@@ -96,7 +96,7 @@ function physicalDescription_Randomizer() {
     } else {
         var task1 = physical_descriptions.at(1)
         var task2 = physical_descriptions.at(3)
-        var task3 = physical_descriptions.at(4) 
+        var task3 = physical_descriptions.at(5) 
         return [task1, task2, task3]
     }
 
@@ -113,7 +113,7 @@ function randomiz_mental() {
     } else {
         var task1 = mental_tasks.at(1)
         var task2 = mental_tasks.at(3)
-        var task3 = mental_tasks.at(4)
+        var task3 = mental_tasks.at(5)
         return [task1, task2, task3]
     }
   }
